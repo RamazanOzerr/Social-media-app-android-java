339dc12b8baddeaa9a2806ae3d87e9b8724e46cb
Ramazan Özer <ozerramazan991@gmail.com>
2023-03-28 01:40:12 +0300

    bug in OnSwipeTouchListener class been fixed search post feature been added double click to like with animation

--- app/src/main/java/com/example/findmypet/Activities/MainActivity.java
+++ app/src/main/java/com/example/findmypet/Activities/MainActivity.java
@@ -71,6 +71,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
 //    @Override
 //    public boolean onCreateOptionsMenu(Menu menu) {
 //        MenuInflater inflater = getMenuInflater();
